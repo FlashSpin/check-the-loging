1716e2b5eaa4ffaf63505bc94b50c6b337f0ac31
Michael Carlton <mccarlton@gmail.com>
2021-04-28 11:22:43 +0100

    added some if and else, not working...

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-if (prompt("hello, who is this?") == "admin"){
-    alert("run");
+let userName = prompt("hello, who is this?");
+
+if (userName === Admin){
+    let password = prompt("Whats the password?");
+
+    if (password === "theMaster"){
+        alert("you're in buddy!");
+    }else{
+        alert("password incorect!");
+    }
+}else if (userName === "" || null){
+    alert("cancled")
 }else{
-    alert("Stop");
+    alert("Try again")
 }
